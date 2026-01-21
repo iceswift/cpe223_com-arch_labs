dee3bf380c224306cb88004bfec379a9476a7bee
Peemmaphat Sripongsai <83326313+SupeemAFK@users.noreply.github.com>
2026-01-21 20:43:28 +0700

    fix: fix variable names and not make sense if else logic

--- Lab2/lab2_instructions sequencing.c
+++ Lab2/lab2_instructions sequencing.c
@@ -2,24 +2,15 @@
 
 int main() {
     int n = 44;
-    long long fib_prev = 0;     // F(0)
-    long long fib_curr = 1;     // F(1)
-    long long fib_next;
-
-    if (n == 0) {
-        printf("Fibonacci(%d) = %lld\n", n, fib_prev);
-        return 0;
-    } else if (n == 1) {
-        printf("Fibonacci(%d) = %lld\n", n, fib_curr);
-        return 0;
-    }
+    long long prev = 0;   
+    long long curr = 1;  
+    long long next;
 
     for (int i = 2; i <= n; i++) {
-        fib_next = fib_prev + fib_curr;
-        fib_prev = fib_curr;
-        fib_curr = fib_next;
+        next = prev + curr;
+        prev = curr;
+        curr = next;
     }
 
-    printf("Fibonacci(%d) = %lld\n", n, fib_curr);
-    return 0;
+    printf("Fibonacci of %d is %lld", n, curr);
 }
